4b422cca1dc989176f18d06e61eefd24e041a51a
vincenttarritheig <vincent.tarrit@heig-vd.ch>
2022-03-17 12:16:56 +0100

    auto

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -91,7 +91,7 @@
         }
 
         #searchBar input {
-            width: 80%;
+            width: 100%;
             padding: 12px 20px;
             margin: 8px 0;
             box-sizing: border-box;

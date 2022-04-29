65ad40e9dbfe2bd5ea75d96c710cf2b4a7804058
Vincent <vincent.tarrit29@gmail.com>
2022-04-29 14:59:16 +0200

    link relative image

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -43,7 +43,7 @@ class Image extends Model
 
     protected function getLinkAttribute()
     {
-        return env("APP_URL") . $this->local_url;
+        return $this->local_url;
         //return env("APP_URL") . "/storage/planes/" . $this->crash_id . "/" . $this->id . ".jpg";
     }
 }

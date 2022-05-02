87d9873aa4be962f72360c5d1da0de53f980a872
Vincent <vincent.tarrit29@gmail.com>
2022-05-02 21:51:03 +0200

    auto

--- resources/js/popupInfo.js
+++ resources/js/popupInfo.js
@@ -3,7 +3,7 @@ import * as $ from "jquery";
 
 d3.select("#infoButton").on("click", function() {
     d3.select("#map-section").style("display", "none");
-    d3.select("#info-section").style("display", "block");
+    d3.select("#info-section").style("display", "flex");
 });
 
 d3.select(".close-info-section").on("click", function() {

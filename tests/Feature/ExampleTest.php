59d23931605f9cc712bfe7a7d303328ca8fdc3f9
Vincent <vincent.tarrit29@gmail.com>
2022-05-03 09:46:36 +0200

    test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -18,4 +18,60 @@ class ExampleTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    public function test_the_application_return_list_incidents()
+    {
+        $response = $this->get('/api/incidents');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_the_application_return_list_incidents_by_date()
+    {
+        $response = $this->get('/api/incidents/2020/01/01');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_the_application_return_list_incidents_by_date_with_invalid_date()
+    {
+        $response = $this->get('/api/incidents/2020/13/01');
+
+        $response->assertStatus(404);
+    }
+
+    public function test_the_application_return_list_incidents_by_date_with_invalid_date_month()
+    {
+        $response = $this->get('/api/incidents/2020/00/01');
+
+        $response->assertStatus(404);
+    }
+
+    public function test_the_application_return_list_incidents_by_date_with_invalid_date_day()
+    {
+        $response = $this->get('/api/incidents/2020/01/00');
+
+        $response->assertStatus(404);
+    }
+
+    public function test_the_application_return_list_incidents_by_date_with_invalid_date_day_and_month()
+    {
+        $response = $this->get('/api/incidents/2020/00/00');
+
+        $response->assertStatus(404);
+    }
+
+    public function test_the_application_return_list_incidents_by_date_with_invalid_date_day_and_month_and_year()
+    {
+        $response = $this->get('/api/incidents/2020/00/00/00');
+
+        $response->assertStatus(404);
+    }
+
+    public function test_the_application_return_list_incidents_with_search()
+    {
+        $response = $this->get('/api/search/test');
+        
+        $response->assertStatus(200);
+    }
 }

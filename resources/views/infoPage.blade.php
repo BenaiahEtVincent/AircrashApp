cb7f34510a7ec5b84d292302816c087277021755
Witizli <benaiah.finkel@heig-vd.ch>
2022-05-03 16:23:06 +0200

    css_clean_infoPage

--- resources/views/infoPage.blade.php
+++ resources/views/infoPage.blade.php
@@ -14,7 +14,7 @@
                                 <li><a href="#NY" class="menuhover">New York</a></li>
                                 <li><a href="#RP" class="menuhover">Rio-Paris</a></li>
                                 <li><a href="#MA" class="menuhover">Malaysie</a></li>
-                                <li><a href="#MAP" class="menuhover">Carte</a></li>
+                                <li><a href="#home" class="close-info-section menuhover">Carte</a></li>
                             </ul>
                         </div>
                     </div>
@@ -49,25 +49,25 @@
             <div class="bg_NY">
                 <h3>11<br>Septembre<br>2001</h3>
             </div>
-            <div id="containerInfoPage" class="sectionContenu history">
-                <div class="bloc_text">
-                    <h4>WORLD TRADE CENTER</h4>
-                    <p>Le 11 septembre, 4 avions décollant de Boston, Washington et New York se font détourner par une
-                        équipe de terroristes. Chaque avion devait s’écraser sur une cible différente du capitalisme
-                        occidental. Le pentagone, les tours jumelles du world trade center et un dernier qui visait soit
-                        la maison blanche soit le capitole. il s’est cependant écrasé dans un champ de pennsylvanie.
-                        Au total, près de 2900 personnes ont perdu la vie dans cet événement. Il s’agit de l'événement
-                        qui a fait bousculer le monde moderne dans la peur du terrorisme et de la sécurité omniprésente
-                        dans les aéroports.
-                    </p>
+            <div class="sectionNY">
+                <div id="containerInfoPage" class="sectionContenu history">
+                    <div class="bloc_text">
+                        <h4>WORLD TRADE CENTER</h4>
+                        <p>Le 11 septembre, 4 avions décollant de Boston, Washington et New York se font détourner par
+                            une
+                            équipe de terroristes. Chaque avion devait s’écraser sur une cible différente du capitalisme
+                            occidental. Le pentagone, les tours jumelles du world trade center et un dernier qui visait
+                            soit
+                            la maison blanche soit le capitole. il s’est cependant écrasé dans un champ de pennsylvanie.
+                            Au total, près de 2900 personnes ont perdu la vie dans cet événement. Il s’agit de
+                            l'événement
+                            qui a fait bousculer le monde moderne dans la peur du terrorisme et de la sécurité
+                            omniprésente
+                            dans les aéroports.
+                        </p>
+                    </div>
+
                 </div>
-                {{-- <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean iaculis finibus augue, auctor
-                    volutpat sem commodo eget. Sed tincidunt lacinia feugiat. Ut non lacus fringilla, mollis lacus id,
-                    pharetra elit. Aliquam non consectetur diam, quis ornare purus. In quis lorem id arcu aliquet
-                    semper. Nullam lacinia turpis scelerisque sem semper, in sollicitudin neque viverra. Duis elementum
-                    semper arcu ut vehicula. Fusce vel maximus quam. Morbi at dapibus eros. Nulla vel sem sed tortor
-                    volutpat bibendum. Vivamus dignissim posuere erat et blandit.
-                </p> --}}
             </div>
             <div class="img_multi_NY"></div>
         </section>
@@ -75,35 +75,34 @@
             <div class="bg_RP bg_ombre">
                 <h3>Vol<br>Air France<br>447</h3>
             </div>
-            <div id="containerInfoPage" class="sectionContenu history">
+            <div class="sectionRioParis">
+                <div id="containerInfoPage" class="sectionContenu history">
 
-                {{-- <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean iaculis finibus augue, auctor
-                    volutpat sem commodo eget. Sed tincidunt lacinia feugiat. Ut non lacus fringilla, mollis lacus
-                    id,
-                    pharetra elit. Aliquam non consectetur diam, quis ornare purus. In quis lorem id arcu aliquet
-                    semper. Nullam lacinia turpis scelerisque sem semper, in sollicitudin neque viverra. Duis
-                    elementum
-                    semper arcu ut vehicula. Fusce vel maximus quam. Morbi at dapibus eros. Nulla vel sem sed tortor
-                    volutpat bibendum. Vivamus dignissim posuere erat et blandit.
-                </p> --}}
-                <div class="bloc_text">
-                    <h4>Crash du Rio-Paris</h4>
-                    <p>La nuit du 31 mai, le vol Air France 447 décolle de Rio de Janeiro en direction de Paris. Après
-                        2h de vol, les pilotes envoient leur dernier message aux contrôleurs du sol. Ils survolent alors
-                        l’Atlantique en quittant les côtes brésiliennes. À 2h10, heure locale, les sondes Pitot gèlent
-                        ce qui fait sauter le pilotage automatique. Le commandant de bord est parti se coucher depuis
-                        quelques minutes et le jeune copilote a pris sa place. Sous l’effet de la peur, ne comprenant
-                        pas pourquoi la vitesse diminue, il tire sur le manche. L’avion monte jusqu’à 12’000 mètres
-                        d’altitude. L’avion décroche et l’alarme d'urgence se déclenche. Dans le cockpit plus personne
-                        ne comprend ce qu'il est en train de se passer. Le commandant de bord est réveillé et ne
-                        comprend rien non plus. Le jeune copilote continue à tirer sur le manche sans que les autres
-                        membres ne le remarque. L’avion chute alors sans aucune retenue pendant plus de 3 minutes et
-                        s’écrase dans l’océan.
-                    </p>
-                    <p>Il a fallu 2 ans aux secouristes pour retrouver les boîtes noires. 228 personnes ont perdu la
-                        vie. Les équipages suivent désormais des entraînements pour réagir aux décrochages en haute
-                        altitude.
-                    </p>
+                    <div class="bloc_text">
+                        <h4>Crash du Rio-Paris</h4>
+                        <p>La nuit du 31 mai, le vol Air France 447 décolle de Rio de Janeiro en direction de Paris.
+                            Après
+                            2h de vol, les pilotes envoient leur dernier message aux contrôleurs du sol. Ils survolent
+                            alors
+                            l’Atlantique en quittant les côtes brésiliennes. À 2h10, heure locale, les sondes Pitot
+                            gèlent
+                            ce qui fait sauter le pilotage automatique. Le commandant de bord est parti se coucher
+                            depuis
+                            quelques minutes et le jeune copilote a pris sa place. Sous l’effet de la peur, ne
+                            comprenant
+                            pas pourquoi la vitesse diminue, il tire sur le manche. L’avion monte jusqu’à 12’000 mètres
+                            d’altitude. L’avion décroche et l’alarme d'urgence se déclenche. Dans le cockpit plus
+                            personne
+                            ne comprend ce qu'il est en train de se passer. Le commandant de bord est réveillé et ne
+                            comprend rien non plus. Le jeune copilote continue à tirer sur le manche sans que les autres
+                            membres ne le remarque. L’avion chute alors sans aucune retenue pendant plus de 3 minutes et
+                            s’écrase dans l’océan.
+                        </p>
+                        <p>Il a fallu 2 ans aux secouristes pour retrouver les boîtes noires. 228 personnes ont perdu la
+                            vie. Les équipages suivent désormais des entraînements pour réagir aux décrochages en haute
+                            altitude.
+                        </p>
+                    </div>
                 </div>
             </div>
             <div class="img_multi_RP"></div>
@@ -112,32 +111,33 @@
             <div class="bg_MA bg_ombre">
                 <h3>Vol Malaysia<br>Airlines 370</h3>
             </div>
-            <div id="containerInfoPage" class="sectionContenu history">
-                <div class="bloc_text">
-                    <h4>MH 370</h4>
-                    <p>L’avion le plus mystérieux de notre ère disparaît des radars le 8 mars 2014 avec 239 personnes à
-                        bord.
-                        Le MH370 décolle de Kuala Lumpur en Malaisie pour rejoindre Pékin. L’avion n’a toujours pas été
-                        retrouvé et de nombreuses théories naissent sur la toile. La plus crédible est la piste d’un
-                        incendie à bord qui se serait éteint automatiquement par le manque d’oxygène en altitude. Mais
-                        rien n’explique que l’avion ait suivi les lignes de frontières. Ces traces n’ont pas alertés les
-                        bases militaires. Ni que les transpondeurs ont été a priori manuellement coupés. De plus, de
-                        nombreux radars n'étaient pas tournés dans le bon sens cette nuit-là. Les recherches n’ont rien
-                        abouti mais vont reprendre en juin 2022. De nombreuses pièces ont été retrouvées sur la côte est
-                        de l'Afrique ainsi qu’à Madagascar ou sur l'île de la Réunion. Le mystère plane, et le restera
-                        jusqu'à la découverte des boîtes noires.
-                    </p>
-                </div>
-                {{-- <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean iaculis finibus augue, auctor
-                    volutpat sem commodo eget. Sed tincidunt lacinia feugiat. Ut non lacus fringilla, mollis lacus
-                    id,
-                    pharetra elit. Aliquam non consectetur diam, quis ornare purus. In quis lorem id arcu aliquet
-                    semper. Nullam lacinia turpis scelerisque sem semper, in sollicitudin neque viverra. Duis
-                    elementum
-                    semper arcu ut vehicula. Fusce vel maximus quam. Morbi at dapibus eros. Nulla vel sem sed tortor
-                    volutpat bibendum. Vivamus dignissim posuere erat et blandit.
-                </p> --}}
+            <div class="sectionMALAY">
+                <div id="containerInfoPage" class="sectionContenu history">
+                    <div class="bloc_text">
+                        <h4>MH 370</h4>
+                        <p>L’avion le plus mystérieux de notre ère disparaît des radars le 8 mars 2014 avec 239
+                            personnes à
+                            bord.
+                            Le MH370 décolle de Kuala Lumpur en Malaisie pour rejoindre Pékin. L’avion n’a toujours pas
+                            été
+                            retrouvé et de nombreuses théories naissent sur la toile. La plus crédible est la piste d’un
+                            incendie à bord qui se serait éteint automatiquement par le manque d’oxygène en altitude.
+                            Mais
+                            rien n’explique que l’avion ait suivi les lignes de frontières. Ces traces n’ont pas alertés
+                            les
+                            bases militaires. Ni que les transpondeurs ont été a priori manuellement coupés. De plus, de
+                            nombreux radars n'étaient pas tournés dans le bon sens cette nuit-là. Les recherches n’ont
+                            rien
+                            abouti mais vont reprendre en juin 2022. De nombreuses pièces ont été retrouvées sur la côte
+                            est
+                            de l'Afrique ainsi qu’à Madagascar ou sur l'île de la Réunion. Le mystère plane, et le
+                            restera
+                            jusqu'à la découverte des boîtes noires.
+                        </p>
+                    </div>
 
+
+                </div>
             </div>
             <div class="img_multi_MA"></div>
         </section>

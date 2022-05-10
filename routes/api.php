5dfb16816b47f8018739aee7359fe79405284578
Vincent <vincent.tarrit29@gmail.com>
2022-05-10 10:53:37 +0200

    clean route api

--- routes/api.php
+++ routes/api.php
@@ -20,18 +20,19 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get("/incidents", [IncidentController::class , 'all']);
+Route::controller(IncidentController::class)->group(function () {
+    Route::get("/incidents", 'all');
 
-Route::get("/incidents/{year}/{month?}/{day?}", [IncidentController::class, "index"])
-    ->where('year', "19[0-9][0-9]|20[0-2][0-9]")
-    ->where('month', "0[1-9]|1[0-2]")
-    ->where('day', "0[1-9]|1[0-9]|2[0-9]|3[0-1]")
-    ->name("incidents-list");
+    Route::get("/incidents/{year}/{month?}/{day?}", "index")
+        ->where('year', "19[0-9][0-9]|20[0-2][0-9]")
+        ->where('month', "0[1-9]|1[0-2]")
+        ->where('day', "0[1-9]|1[0-9]|2[0-9]|3[0-1]")
+        ->name("incidents-list");
+        
+    Route::get("/maps", "maps")->name("maps");
+    Route::get("/search/{input}", "search")->name("search");
+    Route::get("/searchCountryCode/{input}", "searchCountryCode")->name("searchCountryCode");
+});
 
-Route::get("/maps", [IncidentController::class, "maps"])->name("maps");
-
-Route::get("/search/{input}", [IncidentController::class, "search"])->name("search");
-
-Route::get("/searchCountryCode/{input}", [IncidentController::class, "searchCountryCode"])->name("searchCountryCode");
 
 //Route::get("/incident/{id}/image", [IncidentController::class, "setImage"])->name("incident-set-image");
